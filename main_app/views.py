868042e6441d013b5d699e4789218e968f705d4a
charanskumar <charansenthilk@gmail.com>
2024-03-12 14:46:02 -0400

    Create page to view list of all dogs

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+dogs = [
+    {'name': 'Gabi', 'breed': 'Maltese', 'description': 'Friendliest dog ever', 'age': 10},
+    {'name': 'Chloe', 'breed': 'Maltese', 'description': 'Not very friendly :()', 'age': 11},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def dogs_index(request):
+    return render(request, 'dogs/index.html', {
+        'dogs': dogs
+})
