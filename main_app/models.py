738de7695047cbe5340e436c205fd9a133b0aa7d
charanskumar <charansenthilk@gmail.com>
2024-03-17 21:53:29 -0400

    Finish part 5

--- main_app/models.py
+++ main_app/models.py
@@ -8,18 +8,24 @@ MEALS = (
     ('D', 'Dinner')
 )
 # Create your models here.
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+    
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
-
+    toys = models.ManyToManyField(Toy)
     def __str__(self):
         return self.name
-    
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
-    
     def fed_for_today(self):
         return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
     
@@ -30,11 +36,8 @@ class Feeding(models.Model):
         choices=MEALS,
         default=MEALS[0][0]
     )
-
     dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
-
     def __str__(self):
         return f"{self.get_meal_display()} on {self.date}"
-    
     class Meta:
         ordering = ['-date']
